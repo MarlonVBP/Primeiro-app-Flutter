916f78a97122cbdc734492e409322e983e457d7e
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-09-05 20:26:03 -0300

    :sparkless: :feat Configuração do main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'calculator.dart';
+import 'converter_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget{
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Calculator(),
+      home: ConverterScreen(),
     );
   }
 }
